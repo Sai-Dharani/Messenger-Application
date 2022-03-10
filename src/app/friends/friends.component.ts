f4e0f08ce1ae1f220665285d371a2d2e4a4dd79b
Sai Dharani <saidharani.menta@dxc.com>
2022-03-10 16:45:14 +0530

    Modified code

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -16,63 +16,9 @@ export class FriendsComponent implements OnInit {
   tempname: string;
   tempemail: any;
   displayemail: string[] = [];
-
-
   usersData;
-
   data: string;
 
-  Users = [{
-    "id": 5440,
-    "name": "Wanda Lynch",
-    "email": "wanda.lynch@example.com"
-  },
-  {
-    "id": 6228,
-    "name": "Katrina Graves",
-    "email": "katrina.graves@example.com"
-  },
-  {
-    "id": 1654,
-    "name": "Louis Daniels",
-    "email": "louis.daniels@example.com"
-  },
-  {
-    "id": 1631,
-    "name": "Gavin Sullivan",
-    "email": "gavin.sullivan@example.com"
-  },
-  {
-    "id": 9880,
-    "name": "June Martinez",
-    "email": "june.martinez@example.com"
-  },
-  {
-    "id": 8634,
-    "name": "Owen Davis",
-    "email": "owen.davis@example.com"
-  },
-  {
-    "id": 3918,
-    "name": "Megan Harrison",
-    "email": "megan.harrison@example.com"
-  },
-  {
-    "id": 3680,
-    "name": "Joel Thompson",
-    "email": "joel.thompson@example.com"
-  },
-  {
-    "id": 2409,
-    "name": "Dora Rose",
-    "email": "dora.rose@example.com"
-  },
-  {
-    "id": 4477,
-    "name": "Candice Neal",
-    "email": "candice.neal@example.com"
-  }
-  ]
   constructor(private db: AngularFireDatabase,
     private route: Router,
     private chatService: ChatService,
@@ -80,33 +26,8 @@ export class FriendsComponent implements OnInit {
     let user: AngularFireList<any> = db.list('/users');
     user.valueChanges().subscribe(usersObj => {
       user[usersObj.length];
-      // let i = 0;
       this.usersData = usersObj;
       console.log(this.usersData);
-      // for (i = 0; i < usersObj.length; i++) {
-      //   this.tempname = JSON.parse(JSON.stringify(usersObj[i].displayName));
-      //   this.displayname[i] = this.tempname;
-      //   // console.log(this.displayname[i]);
-      // }
-      // i = 0;
-      // for (i = 0; i < usersObj.length; i++) {
-      //   this.tempemail = JSON.parse(JSON.stringify(usersObj[i].email));
-      //   this.displayemail[i] = this.tempemail;
-      //   // console.log(this.displayemail[i]);
-      // }
-      // i = 0;
-      // for (i = 0; i < usersObj.length; i++) {
-      //   this.tempname = JSON.parse(JSON.stringify(usersObj[i].displayName));
-      //   this.tempemail = JSON.parse(JSON.stringify(usersObj[i].email));
-      //   console.log(usersObj[i].displayName);
-
-      //   // this.displays[i].email = this.tempemail;
-      //   // this.displays[i].name = this.tempname;
-      //   // console.log(this.tempemail);
-
-      //   // console.log(this.displays[i].email);
-      // }
-
     });
   }
   chat(user: string) {
